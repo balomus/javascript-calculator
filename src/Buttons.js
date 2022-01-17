0aed9218254bce56122f4b95ceecc944c255ca95
Stephen Mercier <balom.us@gmail.com>
2022-01-17 11:58:46 -0500

    Fixed issues with numbers longer than 1 digit. Refactored some repeated code into independent functions.

--- src/Buttons.js
+++ src/Buttons.js
@@ -10,8 +10,7 @@ const Buttons = (props) => {
         
         if (buttonValue === "AC")
         {
-            props.setOutput("0");
-            props.setFormula("0");
+            updateOutputAndDisplay("0");
             props.setRunningFormula("");
         }
 
@@ -19,8 +18,7 @@ const Buttons = (props) => {
         {
             if (props.output === "0" || isOperator.test(props.output))
             {
-                props.setOutput(buttonValue);
-                props.setFormula(buttonValue);
+                updateOutputAndDisplay(buttonValue);
                 if (isOperator.test(props.output))
                 {
                     props.setRunningFormula(props.runningFormula + props.output);
@@ -28,8 +26,7 @@ const Buttons = (props) => {
             }
             else 
             {
-                props.setOutput(props.output + buttonValue);
-                props.setFormula(props.formula + buttonValue);
+                updateOutputAndDisplay(props.output + buttonValue);
             }
         }
 
@@ -37,13 +34,11 @@ const Buttons = (props) => {
         {
             if (isOperator.test(props.output))
             {
-                props.setOutput("0.");
-                props.setFormula("0.")
+                updateOutputAndDisplay("0.");
                 props.setRunningFormula(props.runningFormula + props.output);
             }
             else{
-                props.setOutput(props.output + buttonValue);
-                props.setFormula(props.formula + buttonValue);
+                updateOutputAndDisplay(props.output + buttonValue);
             }
         }
 
@@ -51,15 +46,13 @@ const Buttons = (props) => {
         {
             if(!isOperator.test(props.output))
             {
-                props.setOutput(buttonValue);
-                props.setFormula(buttonValue);
+                updateOutputAndDisplay(buttonValue);
                 props.setRunningFormula(props.runningFormula + props.output);
             }
 
             else
             {
-                props.setOutput(buttonValue);
-                props.setFormula(buttonValue);
+                updateOutputAndDisplay(buttonValue);
             }
         }
 
@@ -72,51 +65,21 @@ const Buttons = (props) => {
     const calculate = (formula) => 
     {
         console.log(formula);
-        // console.log("Index of / is " + formula.indexOf("/"))
-        // console.log("Index of * is " + formula.indexOf("X"))
-        // console.log("Index of - is " + formula.indexOf("-"))
-        // console.log("Index of + is " + formula.indexOf("+"))
         var numbers = formula.split(isOperator);
-        var operators = formula.split(isNum);
-        var answer;
-
-        operators.shift();
-        operators.pop();
-
-        console.log("Numbers are: " + numbers);
-        console.log("Numbers length is " + numbers.length)
-        
-        console.log("Operators are: " + operators);
-        console.log("Operator length is " + operators.length)
+        var operators = formula.split(isNum).filter(n => n);
 
         for (var i = 0; i < operators.length; i++)
         {
             if (operators[i] == "X")
             {
-                console.log("MULTIPLYING")
-                numbers.splice(i, 2, multiply(numbers[i], numbers[i + 1]));
-                operators.splice(i, 1);
-
-                console.log("Numbers are now: " + numbers);
-                console.log("Numbers length is now " + numbers.length)
-                
-                console.log("Operators are now: " + operators);
-                console.log("Operator length is now " + operators.length)
-
+                getAnswer(numbers, operators, i, multiply);
                 i--;
             }
 
             if (operators[i] == "/")
             {
-                console.log("DIVIDING")
-                numbers.splice(i, 2, divide(numbers[i], numbers[i + 1]));
-                operators.splice(i, 1);
-
-                console.log("Numbers are now: " + numbers);
-                console.log("Numbers length is now " + numbers.length)
-                
-                console.log("Operators are now: " + operators);
-                console.log("Operator length is now " + operators.length)
+                getAnswer(numbers, operators, i, divide);
+                i--;
             }
         }
 
@@ -124,36 +87,29 @@ const Buttons = (props) => {
         {
             if (operators[i] == "+")
             {
-                console.log("ADDING")
-                numbers.splice(i, 2, add(numbers[i], numbers[i + 1]));
-                operators.splice(i, 1);
-
-                console.log("Numbers are now: " + numbers);
-                console.log("Numbers length is now " + numbers.length)
-                
-                console.log("Operators are now: " + operators);
-                console.log("Operator length is now " + operators.length)
-
+                getAnswer(numbers, operators, i, add);
                 i--;
             }
 
             if (operators[i] == "-")
             {
-                console.log("SUBTRACTING")
-                numbers.splice(i, 2, subtract(numbers[i], numbers[i + 1]));
-                operators.splice(i, 1);
-
-                console.log("Numbers are now: " + numbers);
-                console.log("Numbers length is now " + numbers.length)
-                
-                console.log("Operators are now: " + operators);
-                console.log("Operator length is now " + operators.length)
+                getAnswer(numbers, operators, i, subtract);
+                i--;
             }
         }
 
         props.setFormula(props.formula + "=" + numbers[0]);
         props.setOutput(numbers[0]);
 
+    }
+
+    const getAnswer = (numbers, operators, i, operatorFunction) => 
+    {
+        console.log("Numbers: " + numbers + " operators " + operators);
+        console.log(operatorFunction.name + " " + numbers[i] + " and " + numbers[i + 1]);
+
+        numbers.splice(i, 2, operatorFunction(numbers[i], numbers[i + 1]));
+        operators.splice(i, 1);
     }
 
     const divide = (num1, num2) => 
@@ -174,6 +130,12 @@ const Buttons = (props) => {
     const add = (num1, num2) =>
     {
         return Number(num1) + Number(num2);
+    }
+
+    const updateOutputAndDisplay = (str) =>
+    {
+        props.setOutput(str);
+        props.setFormula(str);
     }
 
     return ( 
